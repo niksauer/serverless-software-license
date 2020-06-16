1d4acd34f2c1cd61dd07d380f0e1fcbb566d9c4b
TrafalgarOutlaw <eherwin95@googlemail.com>
2020-06-16 16:37:19 +0200

    Commit for debugging current errors

--- src/lib/storage/FileLicenseStorage.spec.ts
+++ src/lib/storage/FileLicenseStorage.spec.ts
@@ -1,49 +1,57 @@
 import test from 'ava';
 import { FileLicenseStorage } from './FileLicenseStorage';
 import { ILicenseStorage, License } from "../interfaces/manager";
-import { utils } from 'ethers';
-import { LicenseTokenEvent } from '../interfaces/registry';
 
-const validLicenseStorage = new FileLicenseStorage(process.cwd() + "/test-data/validLicense.json");
-const validLicenseStorage2 = new FileLicenseStorage(process.cwd() + "/test-data/validLicense2.json");
-const invalidLicenseStorage = new FileLicenseStorage(process.cwd() + "/test-data/invalidLicense.json");
+
+//const validLicenseStorage2 = new FileLicenseStorage(process.cwd() + "/test-data/validLicense2.json");
+//const invalidLicenseStorage = new FileLicenseStorage(process.cwd() + "/../invalidLicense.json");
 
 test("getLicense() fails if disk contents don't match a license", async (t) => {
-    await t.throwsAsync(invalidLicenseStorage.getLicense());
+  const licenseStorage = new FileLicenseStorage("./test-data/invalidLicense.json");
+  await t.throwsAsync(licenseStorage.getLicense());
 });
 
+/*
 test("getLicense() should return the license object", async (t) => {
-    await t.notThrowsAsync(validLicenseStorage.getLicense());
+  await t.notThrowsAsync(validLicenseStorage.getLicense());
 });
 
-test("setLicense() fails if the contents to be written don't match a License.", async (t) => {
-    const data = "{'address': 'testAddress',´/n'data': 'testData',/n'response': 42/n}";
-    const license: License = JSON.parse(data);
-    t.throws(() => validLicenseStorage.setLicense(license));
+/*
+test("setLicense() should write contents to disk.", async (t) => {
+  const license = await validLicenseStorage.getLicense();
+  await t.notThrows(() => validLicenseStorage2.setLicense(license));
 });
 
+test("setLicense() fails if the contents to be written don't match a license.", async (t) => {
+  const license = ({} as License);
+  await t.throws(() => validLicenseStorage2.setLicense(license));
+});
+
+/*
 test("setLicense() test if the contents written to disk match those retrieved.", async (t) => {
-    const data = "{'address': 'testAddress',´/n'data': 'testData',/n'response': 'testResponse/n}";
-    const license: License = JSON.parse(data);
-    validLicenseStorage2.setLicense(license)
-    const license2 = await validLicenseStorage.getLicense();
-    t.notThrows(() => {
-        if (license != license2 || license.challenge.response != license2.challenge.response) {
-            throw new Error("Contents written to disk does not match those retrieved.")
-        };
-    });
+  const data = '{"address":"testAddress", "data":"testData", "response":"testResponse"}';
+  const license: License = JSON.parse(data);
+  validLicenseStorage2.setLicense(license)
+  const license2 = await validLicenseStorage.getLicense();
+  t.notThrows(() => {
+    if (license != license2 || license.challenge.response != license2.challenge.response) {
+      throw new Error("Contents written to disk does not match those retrieved.")
+    };
+  });
 });
 
-/*test("setLicense() fails if the contents written to disk do not match those retrieved.", async (t) => {
-    const data = "{'address': 'testAddress',´/n'data': 'testData',/n'response': 'testResponse/n}";
-    const wrongData = "{'address': 'testAddress',´/n'data': 'testData',/n'response': 'testResponse2/n}";
-    const license: License = JSON.parse(data);
-    const wrongLicense: License = JSON.parse(wrongData);
-    validLicenseStorage2.setLicense(license)
-    const license2 = await validLicenseStorage.getLicense();
-    t.throws(() => {
-        if (wrongLicense != license2 || wrongLicense.challenge.response != license2.challenge.response) {
-            throw new Error("Contents written to disk does not match those retrieved.");
-        }
-    });
-});*/
+/*
+test("setLicense() fails if the contents written to disk do not match those retrieved2.", async (t) => {
+  const data = "{'address': 'testAddress',\n'data'': 'testData',/n'response': 'testResponse/n}";
+  const wrongData = "{'address': 'testAddress',´/n'data': 'testData',/n'response': 'testResponse2/n}";
+  const license: License = JSON.parse(data);
+  const wrongLicense: License = JSON.parse(wrongData);
+  validLicenseStorage2.setLicense(license)
+  const license2 = await validLicenseStorage.getLicense();
+  t.throws(() => {
+    if (wrongLicense != license2 || wrongLicense.challenge.response != license2.challenge.response) {
+      throw new Error("Contents written to disk does not match those retrieved.");
+    }
+  });
+});
+*/
--- src/lib/storage/FileLicenseStorage.ts
+++ src/lib/storage/FileLicenseStorage.ts
@@ -1,36 +1,54 @@
 import { ILicenseStorage, License } from "../interfaces/manager";
 import fs from "fs";
 import { promisify } from "util";
-import { getJsonWalletAddress } from "ethers/lib/utils";
-import { LicenseManager } from "../LicenseManager";
 
 const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 export class FileLicenseStorage implements ILicenseStorage {
 
-    path: string;
+  // MARK: - Public Properties
+  path: string;
 
-    constructor(path: string) {
-        this.path = path;
+  // MARK: - Initialization
+  constructor(path: string) {
+    this.path = path;
+  }
+
+  // MARK: - Public Methods
+  /**TODO:
+   * @returns return the license object.
+   */
+  async getLicense(): Promise<License> {
+    const data = (await readFileAsync(this.path)).toString();
+    const license: License = JSON.parse(data);
+
+    if (!this.isLicense(license)) {
+      throw new Error("The disk contents don't match a License.");
     }
 
-    async getLicense(): Promise<License> {
+    return license;
+  }
 
-        const data = (await readFileAsync(this.path)).toString();
-        const license: License = JSON.parse(data);
-        if (typeof license.challenge.response != "string")
-            throw new Error("The disk contents don't match a License.");
-        return license;
+  /**
+   * TODO:
+   * @param license
+   */
+  async setLicense(license: License): Promise<void> {
+    if (!this.isLicense(license)) {
+      throw new Error("The contents to be written don't match a License.");
     }
 
-    setLicense(license: License): void {
+    await writeFileAsync(this.path, JSON.stringify(license));
+  }
 
-        if (license.challenge == null || license.challenge.response == null) {
-            throw new Error("The contents to be written don't match a License!");
-        }
-
-        fs.writeFile(this.path, JSON.stringify(license), err => {
-            if (err) throw err;
-        });
-    }
+  // MARK: - Private Methods
+  /**
+   *TODO
+   * @param license
+   * @returns true if given license has all necessary fields set.
+   */
+  private isLicense(license: License): boolean {
+    return (typeof license.challenge.address == "string" && license.challenge.data == "string" && license.challenge.response == "string");
+  }
 }
